331b81c33e38eea55a6522ab5518679326f4980e
tushar@vivobook <tusharbansodkar@gmail.com>
2025-06-18 14:35:39 +0530

    removing focus from searchbar when clicked outside.

--- src/components/shared/SearchBar.jsx
+++ src/components/shared/SearchBar.jsx
@@ -1,17 +1,44 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SearchInput from "./SearchInput";
 import SearchResult from "./SearchResult";
 
 const SearchBar = () => {
   const [result, setResult] = useState([]);
-  // const [showResult, setShowResult] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const inputRef = useRef(null);
+  const searchContainerRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchContainerRef.current &&
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setShowResult(false);
+        inputRef.current.value = "";
+      }
+    };
+
+    if (showResult) {
+      document.addEventListener("mousedown", handleClickOutside, true);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showResult]);
 
   return (
-    <div className="relative top-1/2 -translate-y-1/3 h-full">
-      <SearchInput setResult={setResult} />
-      {/* <SearchResult result={result} /> */}
-      {/* {showResult ? <SearchResult result={result} /> : null} */}
-      {result.length > 0 ? <SearchResult result={result} /> : null}
+    <div
+      ref={searchContainerRef}
+      className="relative top-1/2 -translate-y-1/3 h-full"
+    >
+      <SearchInput
+        inputRef={inputRef}
+        setResult={setResult}
+        setShowResult={setShowResult}
+      />
+      {showResult && result.length > 0 && <SearchResult result={result} />}
     </div>
   );
 };
--- src/components/shared/SearchInput.jsx
+++ src/components/shared/SearchInput.jsx
@@ -2,21 +2,24 @@ import axios from "axios";
 import { Search } from "lucide-react";
 import { useState } from "react";
 
-const SearchInput = ({ setResult }) => {
+const SearchInput = ({ setResult, setShowResult, inputRef }) => {
   const [input, setInput] = useState("");
 
   const handleChange = (e) => {
     let currentValue = e.target.value;
     setInput(currentValue);
-    // setResult(currentValue);
-    // console.log(currentValue);
 
     axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
       const data = res.data;
       const result = data.filter((item) => {
         return item.name.toLowerCase().includes(currentValue.toLowerCase());
       });
-      //   console.log(result);
+
+      if (result.length > 0) {
+        setShowResult(true);
+      } else {
+        setShowResult(false);
+      }
 
       setResult(result);
     });
@@ -27,6 +30,7 @@ const SearchInput = ({ setResult }) => {
       <div className="relative flex justify-around items-center w-80 shadow-md/20 rounded-md">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-amber-800" />
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search any stock or commodity"
           className="w-full h-10 pl-10 pr-4 border rounded-md focus:outline-none  focus:ring-blue-300 bg-gray-50 border-none text-gray-600"
