6dce9ba389551bcfd69af229394a1379308f0145
tushar@vivobook <tusharbansodkar@gmail.com>
2025-07-10 16:38:47 +0530

    added spinning loaders

--- src/components/shared/LoadingSpinner.jsx
+++ src/components/shared/LoadingSpinner.jsx
@@ -2,9 +2,8 @@ import { LoaderPinwheel } from "lucide-react";
 
 const LoadingSpinner = () => {
   return (
-    <div className=" flex items-center justify-center h-full w-full">
+    <div className="flex items-center justify-center h-svh w-full">
       <LoaderPinwheel className="animate-spin text-[#7C444F]" size={40} />
-      <span className="sr-only">Loading...</span>
     </div>
   );
 };
